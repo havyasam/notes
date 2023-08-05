deb5bd64e75dd7ed148fe6a37e1272f55b333f5d
havyas <havyasagaltha@gmail.com>
2023-08-05 13:48:37 +0530

    added some javascript code

---
+++ app.js
@@ -0,0 +1,15 @@
+const buton=document.getElementById("btn")
+const main=documnet.querySelector("#main")
+
+
+function addtext(){
+    document.getElementById("demo").innerHTML=`<div id="logo">
+    <i class="fa-regular fa-pen-to-square">notes</i>
+
+    <div id="notes">
+        <i class="fa-solid fa-trash"></i>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+
+    </div>`
+
+}
