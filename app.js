ed0b23861be69ddae37cc83253e2f33d9ef08e01
havyas <havyasagaltha@gmail.com>
2023-08-09 17:39:12 +0530

    deleted one file

--- app.js
+++ app.js
@@ -1,49 +1,59 @@
-const container=document.getElementById("main")
-const btn=document.getElementById("btn")
+const container = document.getElementById("main");
+const btn = document.getElementById("btn");
 
-btn.addEventListener("click",addtext)
+// Event listener: Triggered when "Add Note" button is clicked
+btn.addEventListener("click", addText);
 
-function addtext(){
-  let note=document.createElement("div")
-  note.innerHTML=`<div id="logo">
-  <i class="fa-regular fa-pen-to-square">notes</i>
+// Load notes from localStorage when the page loads
+loadNotesFromLocalStorage();
 
-  <div id="notes">
-      <i  class="fa-solid fa-trash"></i>
-      <textarea class="hello"name="" id="text" cols="30" rows="10"></textarea>
+// Function: Add a new note with a textarea
+function addText() {
+    // Create a new <div> element for the note
+    let note = document.createElement("div");
+    note.innerHTML = `
+        <div id="logo">
+            <i class="fa-regular fa-pen-to-square">notes</i>
+            <div class="notes">
+                <i class="fa-solid fa-trash"></i>
+                <textarea class="hello" name="" id="text" cols="30" rows="10"></textarea>
+            </div>
+        </div>`;
 
-  </div>`
- const trash=note.querySelector(".fa-trash")
- 
-  trash.addEventListener("click",()=>{
-    note.remove()
-  })
-  
-  container.appendChild(note)
- 
-  
-  
-  
-}
+    // Find the trash icon within the note
+    const trash = note.querySelector(".fa-trash");
 
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode")
-}
-const containers = document.querySelectorAll('.hello');
-    const storageKeyPrefix = 'text';
+    // Event listener: Triggered when the trash icon is clicked
+    trash.addEventListener("click", () => {
+        note.remove();
+    });
 
-    const init = () => {
-      containers.forEach((container, index) => {
-        const textArea = container.querySelector('textarea');
-        const storageKey = `${storageKeyPrefix}-${index}`;
+    // Add the note to the container
+    container.appendChild(note);
 
-        textArea.value = localStorage.getItem(storageKey);
+    // Find the textarea within the note
+    const textarea = note.querySelector('textarea');
 
-        textArea.addEventListener('input', () => {
-          localStorage.setItem(storageKey, textArea.value);
-        });
-      });
+    // Load saved content from localStorage
+    const savedContent = localStorage.getItem('note_' + Date.now());
+    if (savedContent) {
+        textarea.value = savedContent;
     }
 
-    init();
+    // Event listener: Triggered when the content of the textarea changes
+    textarea.addEventListener('input', () => {
+        const content = textarea.value;
+        localStorage.setItem('note_' + Date.now(), content); // Save content to localStorage
+    });
+}
+
+// Function: Load saved content from localStorage for all textareas
+function loadNotesFromLocalStorage() {
+    const notes = document.querySelectorAll(".note textarea");
+    notes.forEach((textarea, index) => {
+        const savedContent = localStorage.getItem('note_' + (index + 1));
+        if (savedContent) {
+            textarea.value = savedContent;
+        }
+    });
+  }
